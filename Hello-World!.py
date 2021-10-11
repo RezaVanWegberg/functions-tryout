dfdf839590e1490937b5f6d28fad7f7de20931a4
Reza <99068159@mydavinci.nl>
2021-10-11 09:07:57 +0200

    berekeningen toegevoegd

---
+++ Hello-World!.py
@@ -0,0 +1,5 @@
+def hello(getal):
+    for x in range(getal):
+        print ("hello world!")
+
+hello(7)
---
+++ berekeningen.py
@@ -0,0 +1,39 @@
+def addition(number1, number2):
+    print(number1 + number2)
+
+addition(10,12)
+
+print('---')
+
+def subtraction(number3, number4):
+    print(number3 - number4)
+
+subtraction(58,34)
+
+print('---')
+
+def multiplication(number4, number5):
+    print(number4 * number5)
+
+multiplication(6,7)
+
+print('---')
+
+def division(number6, number7):
+    print(number6 / number7)
+
+division(144,12)
+
+print('---')
+
+def increment(number8):
+    print (number8 + 1)
+
+increment(12)
+
+print('---')
+
+def decrement(number9):
+    print (number9 - 1)
+
+decrement(34)
